400bb24ddcc00e360e01685c9d8db1d7687583b9
Marvyn95 <marvinluyombya@gmail.com>
2023-04-12 19:55:24 +0300

    Initial commit

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - traverse elements of tree using in-order traversal
+ * @tree: tree to go traverse
+ * @func: function to use for each node
+ * Return: void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - print elements of tree using post-order traversal
+ * @tree: tree to traverse
+ * @func: function to use for each node encountered
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
+
+	func(tree->n);
+}
